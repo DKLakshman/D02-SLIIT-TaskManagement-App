e163e515d4dcf95d7731c690492d60e06c7071e6
DKLakshman <116593030+DKLakshman@users.noreply.github.com>
2024-05-12 17:26:27 +0530

    update function

--- tasco/app/src/main/java/com/example/tasco/NotesAdapter.kt
+++ tasco/app/src/main/java/com/example/tasco/NotesAdapter.kt
@@ -1,22 +1,24 @@
 package com.example.tasco
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class NotesAdapter(private var notes:List<Note>, context:Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>(){
+class NotesAdapter(private var notes: List<Note>, context: Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
 
-    class NoteViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-        val  titleTextView:TextView = itemView.findViewById(R.id.titleTextView)
-        val  contentTextView:TextView = itemView.findViewById(R.id.contentTextView)
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton: AppCompatImageView = itemView.findViewById(R.id.updateButton)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        val view =LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return NoteViewHolder(view)
     }
 
@@ -26,9 +28,16 @@ class NotesAdapter(private var notes:List<Note>, context:Context) : RecyclerView
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
+
+        holder.updateButton.setOnClickListener {
+            val intent = Intent(holder.itemView.context, updateActivity::class.java).apply {
+                putExtra("note_id", note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
-    fun refreshData(newNotes:List<Note>){
+    fun refreshData(newNotes: List<Note>) {
         notes = newNotes
         notifyDataSetChanged()
     }
